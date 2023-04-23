9abdb190b25606d8a91aa3460a157859d3d88d0f
Egor Ryabkov <525286+egor83@users.noreply.github.com>
2023-04-23 20:09:17 +0300

    Remove python_2_unicode_compatible

--- django_messages/models.py
+++ django_messages/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import signals
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+# from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
@@ -45,7 +45,7 @@ class MessageManager(models.Manager):
         )
 
 
-@python_2_unicode_compatible
+# @python_2_unicode_compatible
 class Message(models.Model):
     """
     A private message from user to user
